d4a5c815f9b95da59815537a390df92c2bd43aa1
ArthurscBR <arthuraracati2@gmail.com>
2025-09-24 15:53:27 -0300

    Adicionando strings a uma lista de strings

--- Questão1-ArthurSouza/Program.cs
+++ Questão1-ArthurSouza/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    public static void Main()
+    {
+        List<string> nomes = new List<string>();
+
+        Console.WriteLine("quantidade de nomes que deseja inserir na lista: ");
+        int qtd = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i<qtd; i++)
+        {
+            Console.WriteLine("Digite um nome para a lista: ");
+            string nome = Console.ReadLine();
+            nomes.Add(nome);
+        }
+
+    }
+}
