bbfcd667274f184bbd34f6f3becf101f587b9e93
ArthurscBR <arthuraracati2@gmail.com>
2025-09-25 14:41:39 -0300

    Adicionando várias strings de uma vez à uma lista de string

--- Questão1-ArthurSouza/Program.cs
+++ Questão1-ArthurSouza/Program.cs
@@ -1,10 +1,8 @@
 ﻿
 class Program
 {
-    public static void Main()
+    public static void adiciona(List<string> lista)
     {
-        List<string> nomes = new List<string>();
-
         Console.WriteLine("quantidade de nomes que deseja inserir na lista: ");
         int qtd = int.Parse(Console.ReadLine());
 
@@ -12,8 +10,30 @@ class Program
         {
             Console.WriteLine("Digite um nome para a lista: ");
             string nome = Console.ReadLine();
-            nomes.Add(nome);
+            lista.Add(nome);
+        }
+    }
+
+    public static void adiciona_varios(List<string> lista)
+    {
+        Console.WriteLine("quantidade de nomes que deseja inserir na lista: ");
+        int qtd = int.Parse(Console.ReadLine());
+        string[] adicionaveis = new string[qtd];      
+        for(int i = 0; i<qtd; i++)
+        {
+            Console.WriteLine("Digite um nome para a lista: ");
+            string nome = Console.ReadLine();
+            adicionaveis[i] = nome;
         }
 
+        lista.AddRange(adicionaveis);
+    }
+
+    static void Main()
+    {
+        List<string> nomes = new List<string>() {"Arthur","Heitor"};
+        //adiciona(nomes);
+        adiciona_varios(nomes);
+        nomes.ForEach(item=>{Console.WriteLine(item);});
     }
 }
